42f54ec2695e4aad60a0c440bef6a74278e23e7b
primadonna <jwhyun2215@gmail.com>
2021-12-11 18:42:56 +0900

    config template

---
+++ heyEmoji/common/config.template.py
@@ -0,0 +1,57 @@
+"""
+Please Replace your own
+
+* BOT_USER_OAUTH_TOKEN : slack bot token
+* DB_URL: your database
+
+"""
+
+from dataclasses import dataclass
+from os import environ, path
+
+base_dir = path.dirname(path.dirname(path.dirname(path.abspath(__file__))))
+
+
+@dataclass
+class Config:
+    """
+    Basic Configuration
+    """
+    BASE_DIR = base_dir
+    DB_POOL_RECYCLE: int = 900
+    DB_ECHO: bool = False
+    BOT_USER_OAUTH_TOKEN: str = "BOT_USER_OAUTH_TOKEN"
+    EMOJI: str = ":love:"
+
+
+@dataclass
+class LocalConfig(Config):
+    DB_URL: str = "DB_URL"
+    PROJ_RELOAD: bool = True
+    TRUSTED_HOSTS = ["*"]
+    ALLOW_SITE = ["*"]
+
+
+@dataclass
+class ProdConfig(Config):
+    DB_URL: str = "DB_URL"
+    PROJ_RELOAD: bool = False
+    TRUSTED_HOSTS = ["*"]
+    ALLOW_SITE = ["*"]
+
+
+@dataclass
+class TestConfig(Config):
+    DB_URL: str = "DB_URL"
+    TRUSTED_HOSTS = ["*"]
+    ALLOW_SITE = ["*"]
+    TEST_MODE: bool = True
+
+
+def conf():
+    """
+    Load Configuration
+    :return:
+    """
+    config = dict(prod=ProdConfig(), local=LocalConfig(), test=TestConfig())
+    return config.get(environ.get("API_ENV", "local"))
